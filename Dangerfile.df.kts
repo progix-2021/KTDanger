bb8e3348326d2ff6e2a206968a0c44d9f707c983
progix2021 <79107860+progix2021@users.noreply.github.com>
2021-02-17 23:18:45 +0530

    Update Dangerfile.df.kts

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -24,6 +24,34 @@ danger(args) {
     onGitHub {
         val xmlFile: File = File("detekt-hint-report.xml")
         allSourceFiles.forEach { println(it.toString()) }
+        val xmlDoc: Document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(xmlFile)
+        xmlDoc.documentElement.normalize()
+        val fileList: NodeList = xmlDoc.getElementsByTagName("file")
+        for (i in 0 until fileList.length) {
+            var fileNode = fileList.item(i) as Element
+            
+            val fileName = fileNode.getAttribute("name")
+            println("Filename: $fileName")
+
+            for (k in 0 until fileNode.getElementsByTagName("error").length) {
+                val error = fileNode.getElementsByTagName("error").item(k) as Element
+                println("Error")
+
+                val line = error.getAttribute("line")
+                println("Line: $line")
+                val message = error.getAttribute("message")
+                println("Message: $message")
+
+                if (allSourceFiles.any { fileName.trim().contains(it.trim()) }) {
+                    // Find the sourcefile without the /github/workflow prefix to its path.
+                    val file = allSourceFiles.find { fileName.trim().contains(it.trim()) } ?: fileName
+
+                    // Only notify about the warning if the file has been modified in this PR
+                    println("Adds warning for $fileName")
+                    warn(message, file, line.toInt())
+                }
+            }
+        }
         val isTrivial = pullRequest.title.contains("#trivial")
 
         // Changelog

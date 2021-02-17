aabbc11387b8553e68e20a8b61c01823fcfb2516
progix2021 <79107860+progix2021@users.noreply.github.com>
2021-02-17 23:07:52 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,20 +6,9 @@ export DANGER_GITHUB_API_TOKEN="$1"
 # download detekt jar
 curl -sSLO https://github.com/detekt/detekt/releases/download/v1.8.0/detekt && chmod a+x detekt
 
-# download detekt-hint jar
-curl -L "https://oss.sonatype.org/service/local/artifact/maven/redirect?r=releases&g=io.github.mkohm&a=detekt-hint&v=0.1.5&e=jar" -o detekt-hint-0.1.5.jar
-echo "Should show detekt-hint jar:"
-ls
-
-echo "Running detekt with the detekt-hint plugin ..."
-
 # Run detekt with the detekt-hint plugin, requiring a config file to be set up in the repository. Redirect output to null to not bloat the logs with warning from the type-resolution.
-java -jar detekt --config config/detekt-hint-config.yml --input . -cp . --report xml:detekt-hint-report.xml --includes '**/*.kt' &> /dev/null
+java -jar detekt --input . -cp . --report xml:detekt-hint-report.xml --includes '**/*.kt' 
 
 ls
 
-# Install danger-kotlin
-bash <(curl -s https://raw.githubusercontent.com/danger/kotlin/master/scripts/install.sh)
-source ~/.bash_profile
-
-npx --package danger danger-kotlin ci --dangerfile /Dangerfile.df.kts --verbose
+danger-kotlin ci --dangerfile /Dangerfile.df.kts --verbose
